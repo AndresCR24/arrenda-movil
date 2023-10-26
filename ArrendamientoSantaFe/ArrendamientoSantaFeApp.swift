df40a130144e1891af2851d6b9f4016415ef60c3
AndresCR24 <c-andres-98@hotmail.com>
2023-10-25 21:04:41 -0500

    Initial Commit

---
+++ ArrendamientoSantaFe/ArrendamientoSantaFeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrendamientoSantaFeApp.swift
+//  ArrendamientoSantaFe
+//
+//  Created by Andres David Cardenas Ramirez on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ArrendamientoSantaFeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArrendamientoSantaFe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArrendamientoSantaFe
+//
+//  Created by Andres David Cardenas Ramirez on 25/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
